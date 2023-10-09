c62ce69a3f2cbb95611d8050a8aa7766a68083f0
phihung0707 <33244744+phihung0707@users.noreply.github.com>
2023-10-09 16:53:21 +0700

    Update dotnet.groovy

--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -21,15 +21,10 @@ void call() {
 //========================================================================
 //========================================================================
 
-    stage ('Prepare Package') {
-        script {
-            writeFile file: '.ci/Dockerfile.SDK', text: libraryResource('dev/demo/flows/dotnet/docker/Dockerfile.SDK')
-        }
-    }
-
     stage ("Build Solution") {
-        docker.build("demo/${name}-sdk:${BUILD_NUMBER}", "--force-rm --no-cache -f ./.ci/Dockerfile.SDK \
-        --build-arg BASEIMG=${baseImage} --build-arg IMG_VERSION=${baseTag} ${WORKSPACE}") 
+        sh """
+            docker build --build-arg file_name="${name}" -t "${tag}"  .
+        """
     }
 }
 
